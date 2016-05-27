a7b9cea620e46a4ccc10aeb652e17ab943efb77b
Emiyassstar <emiyasstar@outlook.com>
2016-05-27 18:24:03 +0800

    初始化日志模块

--- app/src/main/java/com/example/emiyasstar/camerademo/MainActivity.java
+++ app/src/main/java/com/example/emiyasstar/camerademo/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.emiyasstar.camerademo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+
+import cn.redcdn.log.CustomLog;
+import cn.redcdn.log.LogcatFileManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG="MainActivity";
+    SurfaceView surfaceView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LogcatFileManager.getInstance().setLogDir("com.example.camerademo");
+        LogcatFileManager.getInstance().start("camerademo");
+        CustomLog.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
+        surfaceView=(SurfaceView) findViewById(R.id.local_video_surfaceview);
+;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CustomLog.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
